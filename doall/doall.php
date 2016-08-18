939744fc37d62b2ffdfe7e21a7d37ebe50e4087e
sum2016 <Lin@Helens-MacBook-Pro.local>
2016-08-18 15:38:16 -0400

    doall plugin v.1 - add custom post type
    adds a custom post type

--- doall/doall.php
+++ doall/doall.php
@@ -11,6 +11,46 @@
 /**
 * Custom Post Type
 */
+function doall_post_type() {
+  register_post_type( 'custom_post',
+    array(
+    	'labels' => array( //customize label texts
+    		'name' => 'Custom Post',
+    		'singular_name' => 'Custom Post',
+    		'add_new' => 'Add Custom Post',
+    		'add_new_item' => 'Add New Custom Post',
+      		'edit_item' => 'Edit Custom Post',
+      		'new_item' => 'New Custom Post',
+      		'view_item' => 'View Custom Post',
+      		'search_items' => 'Search Custom Post',
+      		'not_found' => 'No Custom Post Found',
+      		'not_found_in_trash' => 'No Custom Post has been trashed',
+      		'parent_item_colon' => 'Parent Custom Post',
+      		'all_items' => 'All Custom Post'
+      	),
+      	'public' => true, //controls visibility
+      	'menu_position' => 5, //menu position - 5 sets it below posts
+      	'menu_icon' => 'dashicons-admin-post', //change menu icon to home icon & Reference: https://developer.wordpress.org/resource/dashicons/#portfolio
+      	'hierarchical' => true, // Enables hierarchial structure
+      	'supports' => array( //Enables listed features while creating post
+      		'title',
+      		'editor',
+      		'author',
+      		'thumbnail',
+      		'excerpt'
+      	),
+      	'taxonomies' => array( //Enables category and tags
+      		'category',
+      		'post_tag'
+      	),
+      	'has_archive' => true, //Enables post type archives
+      	'can_export' => false //Disable post export
+	)  
+  );
+
+	
+}
+add_action( 'init', 'doall_post_type' );
 
 /**
 * Widget
