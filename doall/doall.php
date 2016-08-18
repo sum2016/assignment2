aa172e222e53df4cfe6476fcdb2fe24fb0b45c10
sum2016 <Lin@Helens-MacBook-Pro.local>
2016-08-18 14:55:01 -0400

    doall plugin original
    doall plugin original

---
+++ doall/doall.php
@@ -0,0 +1,21 @@
+<?php
+/*
+	* Plugin Name: doall
+	* Plugin URI: https://phoenix.sheridanc.on.ca/~ccit3678/
+	* Description: Assignment 2 - a WordPress plugin that adds a custom post type, a widget, and a shortcode
+	* Author: Hyue Lin Kang
+	* Author URI: https://phoenix.sheridanc.on.ca/~ccit3678/
+	* Version: 1.0
+*/
+
+/**
+* Custom Post Type
+*/
+
+/**
+* Widget
+*/
+
+/**
+* Shortcode
+*/
