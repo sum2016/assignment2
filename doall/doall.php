93d19367c3c4a332b38e914b712a3ae97d0b24d3
sum2016 <Lin@Helens-MacBook-Pro.local>
2016-08-18 23:15:54 -0400

    doall plugin v.2 - shortcodes
    adds shortcodes

--- doall/doall.php
+++ doall/doall.php
@@ -54,8 +54,53 @@ add_action( 'init', 'doall_post_type' );
 
 /**
 * Widget
+* References: https://codex.wordpress.org/
 */
 
 /**
 * Shortcode
+* Reference: https://www.smashingmagazine.com/2012/05/wordpress-shortcodes-complete-guide/
 */
+//Enque Shortcode Styles Sheet
+function plugin_shortcodes_styles(){
+	wp_enqueue_style('plugin-style', plugins_url('/css/style.css', __FILE__));
+}
+add_action('wp_enqueue_scripts', 'plugin_shortcodes_styles');
+
+//Self-Closing Shortcode for Displaying List of Old Post First
+function old_posts_shortcode($atts){
+	extract(shortcode_atts(array(
+		'listitle' => 'Old Post List Title', //heading
+		'posts' => 4, //number of post to display
+	), $atts
+	));
+	$return_list = '<ul class=oldpostlist>';
+	query_posts(array(
+		'ignore_sticky_posts' => true, //hide sticky posts
+		'orderby' => 'date', //order by date
+		'order' => 'ASC', //ascending order - old to new
+		'showposts' => $posts //grab post to display
+	));
+	if (have_posts()) :
+		while (have_posts()) : the_post();
+			$return_list .= '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+		endwhile;
+	endif;
+	$return_list .= '</ul>';
+	wp_reset_query();
+	return '<div class="shortcode"><h2>'. $listitle . '</h2>' . $return_list . '</div>';
+}
+add_shortcode('old-posts', 'old_posts_shortcode');
+
+//Enclosing Shortcode for PDF
+function pdf_shortcode($atts, $url){
+	extract(shortcode_atts(array(
+		'title' => 'The Title', //heading
+		'link' => 'https://www.google.com', //hyperlink text url
+		'linktxt' => 'PDF Link', //hyperlink text
+		'width' => '39em', //width of the iframe
+		'height' => '50em' //height of the iframe
+	), $atts));
+	return '<div class="shortcode"><h2>' . $title . '</h2><p><a href="' . $link . '"class="the-link">' . $linktxt . '</a></p><iframe src="https://docs.google.com/viewer?url=' . $url . '&embedded=true" style="width:' . $width . '; height:' . $height . ';">Your browser does not support iframes</iframe></div>';
+}
+add_shortcode('pdf', 'pdf_shortcode');
